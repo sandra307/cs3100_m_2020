7c841d99161f46d609ae7afad74737766765104e
sandra307 <163575543+sandra307@users.noreply.github.com>
2025-05-17 17:45:52 +0530

    assignment addded

--- assignment7.ml
+++ assignment7.ml
@@ -78,7 +78,7 @@ let rec variables_of_term t =
   match t with
   | C _ -> VarSet.empty                      
   | V v -> VarSet.singleton v               
-  | F (Function (_, y)) ->                
+  | F(Function (_, y)) ->                
       List.fold_left
         (fun acc k-> VarSet.union acc (variables_of_term k))
         VarSet.empty
@@ -115,30 +115,32 @@ assert (variables_of_clause (rule (pred "p" [var_t "X"; var_t "Y"; const_t "a"])
 
 module Substitution = Map.Make(struct type t = variable let compare = Stdlib.compare end)
 (* See API docs for OCaml Map: https://caml.inria.fr/pub/docs/manual-ocaml/libref/Map.S.html *)
-
 let string_of_substitution s =
   "{" ^ (Substitution.fold (fun (Variable v) t s -> s ^ "; " ^ v ^ " -> " ^ (string_of_term t)) s "") ^ "}"
 
+
 let rec substitute_in_term s t =
   match t with
-   C _ -> t
-  | V v -> (try Substitution.find v s with Not_found -> t)
-  | F (Function (n, a)) ->
-      let na = List.map (substitute_in_term s) a in
-      F (Function (n, na))
-
+   C _ -> t|
+   V v->(try Substitution.find v s with Not_found->t)|
+   F(Function(n,a))->let na=List.map(substitute_in_term s) a in F(Function(n,na))
+  
+ 
 let rec substitute_in_predicate s (Predicate (p,l)) =
-Predicate (p, List.map (substitute_in_term s) l)
+Predicate(p,List.map(substitute_in_term s)l)
   
   
 let substitute_in_clause s c =
-  match c with
-   Fact p ->
-      Fact (substitute_in_predicate s p)
-  | Rule (h, b) ->
-      let h1 = substitute_in_predicate s h in
-      let b1 = List.map (substitute_in_predicate s) b in
-      Rule (h1, b1)
+match c with 
+    Fact p->Fact(substitute_in_predicate s p)|
+    Rule (h,b)->let h1=substitute_in_predicate s h in
+    let b1=List.map(substitute_in_predicate s )b in
+    Rule (h1,b1)
+
+
+
+
+  
 
 
 (* 10 points *)
@@ -170,18 +172,39 @@ let c = (rule (pred "p" [var_t "X"; var_t "Y"; const_t "a"]) [pred "q" [var_t "X
 let _ = string_of_clause c
 let _ = string_of_clause (freshen c)
 
+let rec apply_subst s t =
+   match t with
+ C _->t|
+ V v->(match List.assoc_opt v s with
+ Some t'->apply_subst s t'|
+ None->t
+ )|
+F(Function(n,a))->F(Function(n,List.map(apply_subst s )a))
+
 exception Not_unifiable
 
-(* YOUR CODE HERE *)
-raise (Failure "Not implemented")
-
 let mgu t1 t2 = 
-  (* YOUR CODE HERE *)
-  raise (Failure "Not implemented")
-
+let rec unify s t1 t2=
+let t1=substitute_in_term s t1 in
+let t2=substitute_in_term s t2 in
+match t1,t2 with
+C c1,C c2 when c1=c2->s|
+V v1,V v2 when v1=v2->s|
+V v,t|t,V v->
+if t=V v then s 
+else if occurs_check v t then raise Not_unifiable
+else 
+let sub'=Substitution.singleton v t in
+Substitution.fold(fun key value acc->
+Substitution.add key(substitute_in_term sub' value)acc )s sub' |>Substitution.add v t
+|F (Function(f1,a1)),F (Function(f2,a2))->if f1<>f2||List.length a1<>List.length a2 then raise Not_unifiable else
+List.fold_left2 unify s a1 a2 |
+_,_->raise Not_unifiable in
+unify Substitution.empty t1 t2 
 let mgu_predicate (Predicate (p1,l1)) (Predicate (p2,l2)) =
-  (* YOUR CODE HERE *)
-  raise (Failure "Not implemented")
+if p1<>p2||List.length l1<>List.length l2 then raise Not_unifiable else
+List.fold_left2(fun acc t1 t2->mgu(substitute_in_term acc t1)(substitute_in_term acc t2))Substitution.empty l1 l2
+
 
 (* 15 points *)
 assert (string_of_substitution (mgu (var_t "X") (var_t "Y")) = "{; X -> Y}" ||
@@ -210,9 +233,13 @@ assert (Substitution.cardinal u = 3);
 assert (string_of_substitution u = "{; X -> a; Y -> a; Z -> a}");
 
 
-let query ?(limit=10) program goal = 
-  (* YOUR CODE HERE *)
-  raise (Failure "Not implemented")
+
+
+
+
+
+let rec query ?(limit=10) program goal = 
+   
 
 (* 10 points *)
 let p = [fact @@ pred "f" [const_t "a"; const_t "b"]] in
@@ -299,3 +326,9 @@ let g = [append (var_t "X") (var_t "Y") (cons (const_t "1") (cons (const_t "2") 
 
 let g' = query p g in
 assert (List.length g' = 4)
+
+
+
+
+
+
